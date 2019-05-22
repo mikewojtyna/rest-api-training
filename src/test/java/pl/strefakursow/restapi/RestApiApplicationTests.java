ea7e70b9c64a6386b98ad5f1a2779dc5475933f0
Mike Wojtyna <mike@buildmysoftware.pro>
2019-05-22 13:53:49 +0200

    Bootstrap project

---
+++ src/test/java/pl/strefakursow/restapi/RestApiApplicationTests.java
@@ -0,0 +1,11 @@
+package pl.strefakursow.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestApiApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
+}
