64260b435a05bca567b034a4af637304ac45c98c
Mike Wojtyna <mike@buildmysoftware.pro>
2019-05-30 13:55:18 +0200

    Add document REST controller

---
+++ src/main/java/pl/strefakursow/restapi/Document.java
@@ -0,0 +1,20 @@
+package pl.strefakursow.restapi;
+
+public class Document {
+	private String title;
+
+	public Document(String title) {
+		this.title = title;
+	}
+
+	public Document() {
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
