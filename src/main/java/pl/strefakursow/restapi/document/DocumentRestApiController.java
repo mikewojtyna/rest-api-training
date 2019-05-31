285b799bba7c17713cdca2936d405fa7dd930317
Mike Wojtyna <mike@buildmysoftware.pro>
2019-05-31 15:42:47 +0200

    Add methods to find single document and document title

--- src/main/java/pl/strefakursow/restapi/document/DocumentRestApiController.java
+++ src/main/java/pl/strefakursow/restapi/document/DocumentRestApiController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/documents")
@@ -13,8 +14,21 @@ public class DocumentRestApiController {
 	private Collection<Document> documents = new ArrayList<>();
 
 	@GetMapping
-	public Iterable<Document> getDocument() {
+	public Iterable<Document> getDocuments() {
 		return documents;
+	}
+
+	@GetMapping("/{number}/title")
+	public Optional<String> getTitleOfDocument(@PathVariable long number) {
+		return documents.stream()
+			.filter(document -> document.getNumber() == number)
+			.findAny().map(Document::getTitle);
+	}
+
+	@GetMapping("/{number}")
+	public Optional<Document> getDocument(@PathVariable long number) {
+		return documents.stream()
+			.filter(doc -> doc.getNumber() == number).findAny();
 	}
 
 	@PostMapping
