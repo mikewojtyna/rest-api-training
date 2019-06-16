c958649ecba5312d750963bf2ec2932eebc6e730
Mike Wojtyna <mike@buildmysoftware.pro>
2019-06-16 17:26:22 +0200

    Refactor towards L2 maturity level

---
+++ src/main/java/pl/strefakursow/restapi/maturity/methods/DocumentService.java
@@ -0,0 +1,31 @@
+package pl.strefakursow.restapi.maturity.methods;
+
+import org.springframework.web.bind.annotation.*;
+import pl.strefakursow.restapi.document.Document;
+import pl.strefakursow.restapi.maturity.util.DataFixtureUtils;
+
+import java.util.List;
+
+@RestController("documentServiceL2")
+@RequestMapping("/api/maturity/l2/documents")
+public class DocumentService {
+
+	private List<Document> documents =
+		DataFixtureUtils.initDocuments();
+
+	@GetMapping
+	public List<Document> getAllDocuments() {
+		return documents;
+	}
+
+	@PostMapping
+	public void addDocument(@RequestBody Document document) {
+		documents.add(document);
+	}
+
+	@DeleteMapping("/{number}")
+	public void removeDocument(@PathVariable("number") long number) {
+		documents.removeIf(document -> document
+			.getNumber() == number);
+	}
+}
