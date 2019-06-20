cad74841fddafa939f8aa88c8d0c9fbbb867c9b9
Mike Wojtyna <mike@buildmysoftware.pro>
2019-06-20 02:18:53 +0200

    Add versioning examples to l3 maturity

--- src/main/java/pl/strefakursow/restapi/maturity/hateoas/DocumentService.java
+++ src/main/java/pl/strefakursow/restapi/maturity/hateoas/DocumentService.java
@@ -3,24 +3,26 @@ package pl.strefakursow.restapi.maturity.hateoas;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.strefakursow.restapi.document.Document;
 import pl.strefakursow.restapi.maturity.util.DataFixtureUtils;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController("documentServiceL3")
-@RequestMapping("/api/maturity/l3/documents")
+@RequestMapping(value = "/api/maturity/l3/documents", produces = "application"
+	+ "/vnd.strefakursow.v2+hal+json")
 public class DocumentService {
 	private List<Document> documents = DataFixtureUtils.initDocuments();
 
-	@GetMapping(produces = "application/json")
+	@GetMapping(produces = "application/vnd.strefakursow.v1+json")
 	public List<Document> getAllDocumentsWithoutLinks() {
 		return documents;
 	}
@@ -60,7 +62,7 @@ public class DocumentService {
 			.body(resources);
 	}
 
-	@GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+	@GetMapping(produces = "text/vnd.strefakursow.v1+plain")
 	public String getAllTitles() {
 		return documents.stream().map(Document::getTitle)
 			.reduce((acc, curr) -> String.join(",", acc, curr))
@@ -68,6 +70,14 @@ public class DocumentService {
 	}
 
 	@PostMapping
+	public ResponseEntity<?> addDocumentV2(@RequestBody Document document) {
+		documents.add(document);
+		return ResponseEntity.created(URI.create(
+			mapToResource(document).getLink("self").getHref()))
+			.build();
+	}
+
+	@PostMapping(produces = "application/vnd.strefakursow.v1+json")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void addDocument(@RequestBody Document document) {
 		documents.add(document);
